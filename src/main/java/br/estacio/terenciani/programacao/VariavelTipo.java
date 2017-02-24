d69cfaff63c5b0ec4527c82f18c8789cac638244
aluno <aluno@mscgresl00401.CGR.ACAD>
2017-02-23 21:30:56 -0300

    Testando Funções(Método)

---
+++ src/main/java/br/estacio/terenciani/programacao/VariavelTipo.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.terenciani.programacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class VariavelTipo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        imprime();
+        
+        System.out.println("Estou Brincando de Funções");
+        
+        imprime();
+    
+    }
+    public static void imprime (){
+        System.out.println("-----");
+        System.out.println("Teste");
+        System.out.println("-----");
+    }
+}
