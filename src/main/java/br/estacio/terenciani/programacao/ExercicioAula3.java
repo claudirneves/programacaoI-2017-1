5c49e793e2934956785f944194ac000b4ba1f4c3
aluno <aluno@mscgresl00403.CGR.ACAD>
2017-03-02 21:31:26 -0300

    Exercicio Aula 3

---
+++ src/main/java/br/estacio/terenciani/programacao/ExercicioAula3.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.terenciani.programacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExercicioAula3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte minhaIdade = 29;
+        String meuNome = "Claudir Neves";
+        float meuPeso = 68f;
+        char tipoSangueneo = '+';
+       // Date meuAniversario = "19/02/1988";
+        byte numeroCalco = 42;
+        boolean casodo = false;
+        System.out.println("Idade : " + minhaIdade);
+    }
+    
+}
