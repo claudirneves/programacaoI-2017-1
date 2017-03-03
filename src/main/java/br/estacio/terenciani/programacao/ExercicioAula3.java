5a4ea5376079d0ff5b2d0c8f2b1d903a9d25458d
aluno <aluno@mscgresl00403.CGR.ACAD>
2017-03-03 20:45:27 -0300

    Exercicio Aula 3 (Testando Funções)

--- src/main/java/br/estacio/terenciani/programacao/ExercicioAula3.java
+++ src/main/java/br/estacio/terenciani/programacao/ExercicioAula3.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package br.estacio.terenciani.programacao;
+import java.util.Date;
 
 /**
  *
@@ -19,10 +20,37 @@ public class ExercicioAula3 {
         String meuNome = "Claudir Neves";
         float meuPeso = 68f;
         char tipoSangueneo = '+';
-       // Date meuAniversario = "19/02/1988";
+        String meuAniversario = "19/02/1988";
         byte numeroCalco = 42;
-        boolean casodo = false;
-        System.out.println("Idade : " + minhaIdade);
+        boolean casado = false;
+        System.out.println("Minha Iddade é: " + idade(minhaIdade));
+        System.out.println("Meu Nome é: " + nome(meuNome));
+        System.out.println("Meu Peso é: " + peso(meuPeso));
+        System.out.println("Meu Aniversário é: " + niver(meuAniversario));
+        System.out.println("Eu Calço: " + calco(numeroCalco));
+        System.out.println("Estado Civil: " + solteiro(casado));
+        
+    }
+    public static byte idade(byte minhaIdade){
+        return minhaIdade;
+    }
+    public static String nome(String meuNome){
+        return meuNome;
+    }
+    public static float peso(float meuPeso){
+        return meuPeso;
+    }
+    public static String niver(String meuAniversario){
+        return meuAniversario;
+    }
+    public static byte calco(byte numeroCalco){
+        return numeroCalco;
+    }
+    public static boolean solteiro(boolean casado){
+        return casado;
+        /*if(casado == false){
+            System.out.println("Solteiro");
+        }*/
     }
     
 }
